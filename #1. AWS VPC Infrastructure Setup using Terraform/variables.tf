9316c66b22bd71b79d5e389743513e06cb0fe943
Mohamed Akram <153831977+Mohamed-Akram26@users.noreply.github.com>
2025-04-06 00:02:14 +0530

    Update variables.tf

---
+++ #1. AWS VPC Infrastructure Setup using Terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "my-instance-type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "client_name" {
+    default = "akram"
+}
+
+variable "Managed_by" {
+    default = "terraform"
+}
